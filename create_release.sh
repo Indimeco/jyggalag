ec6b2d5a3a53d39345089b75836263f1b0fe9f6b
Indimeco <18373729+Indimeco@users.noreply.github.com>
2024-06-02 16:33:21 +1000

    add release script to vc

---
+++ create_release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+package_name="jyggalag"
+	
+# might have to add "android/arm" but requires additional config
+platforms=("darwin/amd64" "linux/amd64")
+
+for platform in "${platforms[@]}"
+do
+	platform_split=(${platform//\// })
+	GOOS=${platform_split[0]}
+	GOARCH=${platform_split[1]}
+	output_name='./build/'$package_name'-'$GOOS'-'$GOARCH
+	if [ $GOOS = "windows" ]; then
+		output_name+='.exe'
+	fi	
+
+	env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+	if [ $? -ne 0 ]; then
+   		echo 'An error has occurred! Aborting the script execution...'
+		exit 1
+	fi
+done
+
